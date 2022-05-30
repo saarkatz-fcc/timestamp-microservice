94d9d097efdce65092edda87517a325384a1ed44
Saar Katz <kats.saar@gmail.com>
2022-05-30 19:29:53 +0300

    Add microservice boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-// happy coding 👻
-console.log("hello world");
+import express from 'express';
+import cors from 'cors'
+import { AddressInfo } from 'net';
+
+// init project
+let app = express();
+
+// enable cors
+app.use(cors({optionsSuccessStatus: 200}));
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// configure api endpoints
+app.get("/api/hello", function (req, res) {
+    res.json({greeting: 'hello API'});
+});
+
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT, function () {
+    const port = (listener.address() as AddressInfo).port
+    if (port) {
+        console.log('Your app is listening on port ' + port);
+    }
+    else {
+        console.log('Your app failed to start!');
+    }
+});
